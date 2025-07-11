8bf534f0b4809afd79a1b2455d8bf74e1c65dbe2
LinkaFilip <filip.linka@email.cz>
2025-07-11 19:18:19 +0000

    changes

--- main/work/projects.js
+++
@@ -1,7 +0,0 @@
-const projects = [
-  {
-    title: "Wrongly developed dreams or 4 harbingers of strangled sadness or when angels deserve only death",
-    image: "/images/wrongly_developed_dreams.webp",
-    link: "/wrongly_developed_dreams/"
-  }
-];
