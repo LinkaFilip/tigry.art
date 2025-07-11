8da8f430146be8aa4f9588abf2d27dda7d1189f7
LinkaFilip <filip.linka@email.cz>
2025-07-11 16:53:47 +0000

    changes

--- main/work/projects.js
+++ main/work/projects.js
@@ -3,33 +3,5 @@ const projects = [
     title: "Wrongly developed dreams or 4 harbingers of strangled sadness or when angels deserve only death",
     image: "/images/wrongly_developed_dreams.webp",
     link: "/wrongly_developed_dreams/"
-  },
-  {
-    title: "Ponor",
-    image: "/images/Ponor-small.webp",
-  },
-  {
-    title: "Mandator",
-    image: "/images/Mandator-small.webp",
-    link: "/mandator/"
-  },
-  {
-    title: "Arlington",
-    image: "/images/Arlington-small.webp",
-  },
-  {
-    title: "The last one before bedtime",
-    image: "/images/the_last_one_before_bedtime.webp",
-    link: "/the_last_one_before_bedtime/"
-  },
-  {
-    title: "Makulatura",
-    image: "/images/makulaturaquality=2.webp",
-    link: "/makulatura/"
-  },
-  {
-    title: "White puppies",
-    image: "/images/white_puppiesquality=2.webp",
-    link: "/white_puppies/"
   }
 ];
