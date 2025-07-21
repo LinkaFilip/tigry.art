468ccc5cc4b091b718ecec1a0c6845327d00225b
LinkaFilip <filip.linka@email.cz>
2025-07-21 20:04:33 +0200

    change

---
+++ main/checkout/checkout.js
@@ -0,0 +1,16 @@
+import { products } from '..posters/products.js';
+
+const selectedProduct = products.find(p => p.id === 'poster001');
+
+const lineItem = {
+  price_data: {
+    currency: selectedProduct.currency,
+    product_data: {
+      name: selectedProduct.name,
+      description: selectedProduct.description,
+      images: [selectedProduct.image],
+    },
+    unit_amount: selectedProduct.price,
+  },
+  quantity: 1
+};
---
+++ main/posters/prducts.js
@@ -0,0 +1,18 @@
+export const products = [
+  {
+    id: 'poster001',
+    name: 'Plakát tygra',
+    description: 'Stylový plakát s ilustrací tygra na kvalitním papíře.',
+    image: '/images/tiger.jpg',
+    price: 1000, // cena v centách nebo haléřích
+    currency: 'eur',
+  },
+  {
+    id: 'poster002',
+    name: 'Plakát pantera',
+    description: 'Elegantní černobílý plakát pantera, vhodný do moderního interiéru.',
+    image: '/images/panther.jpg',
+    price: 1200,
+    currency: 'eur',
+  }
+];
