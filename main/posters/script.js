a90847159790862668ea29a24d9fa890d046e66a
LinkaFilip <filip.linka@email.cz>
2025-07-23 12:21:35 +0200

    creating shopping cart

--- main/posters/script.js
+++ main/posters/script.js
@@ -1,17 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const element = document.querySelector(".CartIndicator_icon__AFivB, .CartIndicator_closeBtn___fEN6");
+  const element = document.querySelector(".Header_cartIndicator__ti8f6, .CartIndicator_icon__AFivB, .CartIndicator_closeBtn___fEN6");
   if (!element) return;
 
-  // SVG kontejner
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   svg.setAttribute("width", "24");
   svg.setAttribute("height", "24");
   svg.setAttribute("viewBox", "0 0 24 24");
 
-  // Křížek
+  
   const crossPath = `<svg fill="none" viewBox="0 0 14 14"><path stroke="currentColor" stroke-linejoin="round" d="M13 1L1 13M1 1l12 12"></path></svg>`;
 
-  // Tvoje vlastní ikona
   const originalPath = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                 <g clip-path="url(#a)">
                   <path stroke="currentColor" stroke-width="1.5" d="M5.5 6.25v-2a3.5 3.5 0 1 1 7 0v2m-11.75.5h16.5v10.5H.75V6.75Z"></path>
@@ -25,11 +23,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let showingCross = false;
 
-  // Vložit výchozí SVG ikonu
   svg.innerHTML = originalPath;
   element.appendChild(svg);
 
-  // Toggle třídy + SVG
   element.addEventListener('click', () => {
     if (element.classList.contains("CartIndicator_closeBtn___fEN6")) {
       element.classList.replace("CartIndicator_closeBtn___fEN6", "CartIndicator_icon__AFivB");
