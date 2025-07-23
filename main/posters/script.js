175b875c20c4e13aef7ff80ce5fa02b2755fba4e
LinkaFilip <filip.linka@email.cz>
2025-07-23 13:33:34 +0200

    creating shopping cart

--- main/posters/script.js
+++ main/posters/script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
   svg.setAttribute("width", "24");
   svg.setAttribute("height", "24");
   svg.setAttribute("viewBox", "0 0 24 24");
-  svh.setAttribute("fill", "none");
+  svg.setAttribute("fill", "none");
 
   const crossPath = `<path stroke="currentColor" stroke-linejoin="round" d="M13 1L1 13M1 1l12 12"></path>`;
   const originalPath = `<path stroke="currentColor" stroke-width="1.5" d="M5.5 6.25v-2a3.5 3.5 0 1 1 7 0v2m-11.75.5h16.5v10.5H.75V6.75Z"></path>`;
