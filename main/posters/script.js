7e2d4b5cadd17494507cb7f2c524d0210a2030a2
LinkaFilip <filip.linka@email.cz>
2025-07-23 12:11:50 +0200

    creating shopping cart

--- main/posters/script.js
+++ main/posters/script.js
@@ -12,7 +12,16 @@ document.addEventListener("DOMContentLoaded", function () {
   const crossPath = `<path stroke="currentColor" stroke-width="2" d="M6 6L18 18M18 6L6 18" />`;
 
   // Tvoje vlastní ikona
-  const originalPath = `<path stroke="currentColor" stroke-width="1.5" d="M5.5 6.25v-2a3.5 3.5 0 1 1 7 0v2m-11.75.5h16.5v10.5H.75V6.75Z"></path>`;
+  const originalPath = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                <g clip-path="url(#a)">
+                  <path stroke="currentColor" stroke-width="1.5" d="M5.5 6.25v-2a3.5 3.5 0 1 1 7 0v2m-11.75.5h16.5v10.5H.75V6.75Z"></path>
+                </g>
+                <defs>
+                  <clipPath id="a">
+                    <path fill="#fff" d="M0 0h18v18H0z"></path>
+                  </clipPath>
+                </defs>
+              </svg>`;
 
   let showingCross = false;
 
