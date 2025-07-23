f1427d8988dc0534b124e5027f96b2c1241bae79
LinkaFilip <filip.linka@email.cz>
2025-07-23 11:54:19 +0200

    creating shopping cart

--- main/posters/script.js
+++ main/posters/script.js
@@ -1,7 +1,11 @@
 var elementIsClicked = false; // declare the variable that tracks the state
-var element = document.querySelector(".CartIndicator_icon__AFivB");
-element.addEventListener('click', clickHandler);
+document.addEventListener("DOMContentLoaded", function () {
+  const element = document.querySelector(".CartIndicator_icon__AFivB");
+  if (element) {
+    element.addEventListener('click', clickHandler);
+  }
+});
 
 function clickHandler() {
-  element.classList.toggle(".CartIndicator_closeBtn___fEN6");
+  element.classList.toggle("CartIndicator_closeBtn___fEN6");
 }
