f12c5107825d75aa1409e36a9087779bc76472b9
LinkaFilip <filip.linka@email.cz>
2025-07-23 22:11:13 +0200

    adding footer checkout

--- main/posters/script.js
+++ main/posters/script.js
@@ -151,6 +151,10 @@ cart.forEach(item => {
   `;
   panel.appendChild(div);
 });
+const totalDiv = document.createElement("div");
+totalDiv.className = "Cart_total";
+totalDiv.innerHTML = `<hr><strong>Celkem: ${total.toFixed(2)} €</strong>`;
+  panel.appendChild(totalDiv);
 renderFooter(total);
 
 }
