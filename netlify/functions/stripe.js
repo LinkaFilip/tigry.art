31832a31d5f6cb24c01f50f22e8efc2d17b78807
LinkaFilip <filip.linka@email.cz>
2025-07-24 20:40:44 +0200

    change

--- netlify/functions/stripe.js
+++ netlify/functions/stripe.js
@@ -1,6 +1,7 @@
 // Manage your variables with style: https://www.netlify.com/blog/2021/07/12/managing-environment-variables-from-your-terminal-with-netlify-cli/
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
-
+console.log("Creating session...");
+console.log(session.id);
 exports.handler = async (event, context) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
