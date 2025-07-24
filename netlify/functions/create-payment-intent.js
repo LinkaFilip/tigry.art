4e0960fe87c0bf04f9b38a794e0fe43c56455acd
LinkaFilip <filip.linka@email.cz>
2025-07-24 21:01:31 +0200

    changes

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,14 +1,22 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY); // ve .env uložíš tajný klíč
 
-exports.handler = async function(event) {
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: 1000, // 10 EUR v centech
-    currency: 'eur',
-    payment_method_types: ['card'],
-  });
+exports.handler = async function (event, context) {
+  const { amount } = JSON.parse(event.body);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
-  };
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "czk",
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
 };
