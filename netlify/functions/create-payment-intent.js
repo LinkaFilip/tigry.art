9b9a325f5d30b9c6d56c9ef1205e941cd787cf63
LinkaFilip <filip.linka@email.cz>
2025-07-24 16:39:11 +0200

    change

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,40 +1,14 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const { products } = require('./data/products');
 
-exports.handler = async function(event, context) {
-  try {
-    const { productId, quantity = 1 } = JSON.parse(event.body);
+exports.handler = async function(event) {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1000, // 10 EUR v centech
+    currency: 'eur',
+    payment_method_types: ['card'],
+  });
 
-    const product = products.find(p => p.id === productId);
-    if (!product) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: 'Produkt nenalezen.' }),
-      };
-    }
-
-    const amount = product.price * quantity;
-
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency: product.currency,
-      payment_method_types: ['card'],
-      metadata: {
-        product_id: product.id,
-        quantity: String(quantity),
-      }
-    });
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        clientSecret: paymentIntent.client_secret,
-      }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: error.message }),
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+  };
 };
