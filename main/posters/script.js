889881091839354c9809975ee2da0fc3908c33c5
LinkaFilip <filip.linka@email.cz>
2025-07-25 21:59:33 +0200

    changes

--- main/posters/script.js
+++ main/posters/script.js
@@ -23,50 +23,43 @@ document.addEventListener("DOMContentLoaded", () => {
   cartPanel.style.transform = isMobile ? "translate(0, -100%)" : "translate(100%, 0)";
   cartPanel.style.transition = "transform 0.5s ease";
 
-
-
-
-
-
-const iconElement = document.querySelector('.CartIndicator_icon__AFivB');
-
-const bagSVG = `
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 18 18">
-  <path stroke="currentColor" stroke-width="1.5"
-    d="M5.5 6.25v-2a3.5 3.5 0 1 1 7 0v2m-11.75.5h16.5v10.5H.75V6.75Z"/>
+  const bagSVG = `
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+  <g clip-path="url(#a)">
+    <path stroke="currentColor" stroke-width="1.5"
+      d="M5.5 6.25v-2a3.5 3.5 0 1 1 7 0v2m-11.75.5h16.5v10.5H.75V6.75Z" />
+  </g>
+  <defs>
+    <clipPath id="a">
+      <path fill="#fff" d="M0 0h18v18H0z"></path>
+    </clipPath>
+  </defs>
 </svg>`;
 
-const crossSVG = `
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 14 14">
+  const crossSVG = `
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
   <path stroke="currentColor" stroke-linejoin="round" d="M13 1L1 13M1 1l12 12"/>
 </svg>`;
 
-let showingBag = true;
-iconElement.innerHTML = bagSVG;
-
-iconElement.addEventListener('click', () => {
-  iconElement.innerHTML = showingBag ? crossSVG : bagSVG;
-  showingBag = !showingBag;
-});
-
-
-const originalPath = `<path stroke="currentColor" stroke-width="1.5"d="M5.5 6.25v-2a3.5 3.5 0 1 1 7 0v2m-11.75.5h16.5v10.5H.75V6.75Z"/>`;
-const crossPath = `<path stroke="currentColor" stroke-linejoin="round" d="M13 1L1 13M1 1l12 12"/>`;
+  // Inicializuj SVG
+  element.innerHTML = bagSVG;
 
   element.addEventListener("click", () => {
-    const isOpen = element.classList.contains("CartIndicator_closeBtn___fEN6");
     const overlay = document.querySelector(".Overlay_overlay__hwjQ3");
+    const isOpen = element.classList.contains("CartIndicator_closeBtn___fEN6");
 
     if (isOpen) {
       element.classList.replace("CartIndicator_closeBtn___fEN6", "CartIndicator_icon__AFivB");
-      svg.innerHTML = originalPath;
+      element.innerHTML = bagSVG;
+
       overlay.style.opacity = "0";
       overlay.style.visibility = "hidden";
       overlay.style.pointerEvents = "none";
       cartPanel.style.transform = isMobile ? "translate(0, -100%)" : "translate(100%, 0)";
     } else {
       element.classList.replace("CartIndicator_icon__AFivB", "CartIndicator_closeBtn___fEN6");
-      svg.innerHTML = crossPath;
+      element.innerHTML = crossSVG;
+
       overlay.style.opacity = "1";
       overlay.style.visibility = "inherit";
       overlay.style.pointerEvents = "auto";
