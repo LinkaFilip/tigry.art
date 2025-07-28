f398ce450fe8cbd6fae541283ad9424f1d49ae6f
LinkaFilip <filip.linka@email.cz>
2025-07-28 12:23:43 +0200

    change

--- main/posters/script.js
+++ main/posters/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
         id: button.dataset.id,
         name: button.dataset.name,
         price: parseFloat(button.dataset.price),
-        image: button.datase.image,
+        image: button.datase.img,
       };
       addToCart(product);
 
