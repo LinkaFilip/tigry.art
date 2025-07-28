727dc71973d3e8ff29202fd1807f386984c345ee
LinkaFilip <filip.linka@email.cz>
2025-07-28 20:05:30 +0200

    details03

--- main/checkout/checkout.js
+++ main/checkout/checkout.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", async () => {
   const SHIPPING_COST = {
-    AU: 1200, AT: 900, BE: 900, CA: 1400, CZ: 500, DK: 1000, FI: 1000, FR: 900, DE: 800,
-    HK: 1500, IE: 1000, IL: 1300, IT: 900, JP: 1500, MY: 1500, NL: 900, NZ: 1400, NO: 1200,
-    PL: 800, PT: 900, SG: 1500, KR: 1500, ES: 900, SE: 1000, CH: 1000, AE: 1500, GB: 1000, US: 1200,
+    AU: 12, AT: 9, BE: 9, CA: 14, CZ: 5, DK: 10, FI: 10, FR: 9, DE: 8,
+    HK: 15, IE: 10, IL: 13, IT: 9, JP: 15, MY: 15, NL: 9, NZ: 14, NO: 12,
+    PL: 8, PT: 9, SG: 15, KR: 15, ES: 9, SE: 10, CH: 10, AE: 15, GB: 10, US: 12,
   };
 
   let stripe;
