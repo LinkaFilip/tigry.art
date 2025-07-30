b9f5db387a8d43d6223e051c3b2e2f25cf442752
LinkaFilip <filip.linka@email.cz>
2025-07-30 23:32:23 +0200

    details11

--- main/checkout/checkout.js
+++ main/checkout/checkout.js
@@ -198,8 +198,7 @@ function applyDiscount(price) {
 }
 
 const containerIfMobile = document.querySelector("._19gi7yt0._19gi7yt12._19gi7yt1a._19gi7yt1l");
-containerIfMobile.textContent = "";
-containerIfMobile.textContent = calculateSubtotal();
+containerIfMobile.textContent = `EUR ${(calculateSubtotal() / 100).toFixed(2)}`;
 
 const subtotal = calculateSubtotal();
 const shipping = getSelectedShipping();
