153e0fd2993507c4e9e873d54e28446ff6d73f52
LinkaFilip <filip.linka@email.cz>
2025-07-30 23:40:44 +0200

    details11

--- main/checkout/checkout.js
+++ main/checkout/checkout.js
@@ -175,7 +175,7 @@ promoInput.addEventListener("input", updatePrices);
     updatePrices();
   });
 const containerIfMobile = document.querySelector("._19gi7yt0._19gi7yt12._19gi7yt1a._19gi7yt1l");
-containerIfMobile.textContent = `EUR ${calculateSubtotal().toFixed(2) + shippingSummary().toFixed(2)}`;
+containerIfMobile.textContent = `EUR ${calculateSubtotal().toFixed(2) + getSelectedShipping().toFixed(2)}`;
   // Handle card payment
   payButton.addEventListener("click", async () => {
     payButton.disabled = true;
