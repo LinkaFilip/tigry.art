2edc20665fcf0ea1c4a1867ee1b9c04e7a83d90f
LinkaFilip <filip.linka@email.cz>
2025-08-04 10:29:00 +0200

    changes

--- main/checkout/checkout.js
+++ main/checkout/checkout.js
@@ -271,3 +271,21 @@ const totalAfterDiscount = applyDiscount(totalBeforeDiscount);
   });
 updatePrices();
 });
+
+document.getElementById("packeta-button").addEventListener("click", () => {
+  Packeta.Widget.pick(
+    {
+      webUrl: "https://www.zasilkovna.cz",
+      country: "cz",
+      language: "cs",
+      zpointId: null,
+    },
+    function(point) {
+      if (point) {
+        document.getElementById("packeta-branch-id").value = point.id;
+        document.getElementById("packeta-button").innerText = `Zvoleno: ${point.name}`;
+        console.log("Zvolené výdejní místo:", point);
+      }
+    }
+  );
+});
