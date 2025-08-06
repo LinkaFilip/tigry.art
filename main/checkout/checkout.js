9c74d4f8961a6dd7c1e2c6396fbf9500389931aa
LinkaFilip <filip.linka@email.cz>
2025-08-06 22:34:16 +0200

    changes

--- main/checkout/checkout.js
+++ main/checkout/checkout.js
@@ -86,7 +86,7 @@ function updateUI() {
     payButton.style.margin = "0px 0px 0px 0px";
     const element = document.querySelector("._1fragemui._1fragemq6._1fragemqc._1fragemqo._1fragemqi._1fragem32._1fragemg9._1fragemi2._1fragemeg._1fragemjv._1fragemms");
     const shownElement = document.querySelector(".jHvVd");
-    shownElement.style.display = "none";
+    shownElement.style.display = "block";
     element.style.borderRadius = "8px";
     element.style.border = "1px solid black";
     const space = document.querySelector("._1fragem32._1fragemms.gfFXW:nth-child(2)");
