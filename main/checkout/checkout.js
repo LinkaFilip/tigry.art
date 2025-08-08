0bf776e4142d31494f21bf2c2405efd50708522f
LinkaFilip <filip.linka@email.cz>
2025-08-08 14:16:30 +0200

    changes

--- main/checkout/checkout.js
+++ main/checkout/checkout.js
@@ -110,8 +110,10 @@ function updateUI() {
 
     payButton.disabled = !localStorage.getItem("selectedBranchId");
     payButton.style.margin = "0px 0px 0px 0px";
+
     container.style.pointerEvents = "visible";
     container.style.opacity = "1";
+
     const shownElement = document.querySelector(".jHvVd");
     shownElement.style.display = "none";
     const all = document.querySelectorAll("._1fragem32._1fragemms.gfFXW");
@@ -128,6 +130,9 @@ function updateUI() {
     localStorage.removeItem("shippingFee");
     payButton.disabled = false;
     payButton.style.margin = "0px 0px 0px 0px";
+    
+    container.style.pointerEvents = "visible";
+    container.style.opacity = "1";
     const element = document.querySelector("._1fragemui._1fragemq6._1fragemqc._1fragemqo._1fragemqi._1fragem32._1fragemg9._1fragemi2._1fragemeg._1fragemjv._1fragemms");
     const shownElement = document.querySelector(".jHvVd");
     shownElement.style.display = "block";
