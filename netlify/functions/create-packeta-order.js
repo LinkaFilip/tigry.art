d3550527ff2d4076b84b590f855565ccb6e783be
LinkaFilip <filip.linka@email.cz>
2025-08-09 21:19:10 +0200

    changes

--- netlify/functions/create-packeta-order.js
+++ netlify/functions/create-packeta-order.js
@@ -1,61 +1,6 @@
 exports.handler = async function(event, context) {
-  if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Method Not Allowed" };
-  }
-
-  try {
-    const data = JSON.parse(event.body);
-
-    const apiPassword = process.env.PACKETA_API_PASSWORD;
-    if (!apiPassword) {
-      console.error("Chybí PACKETA_API_PASSWORD v env proměnných");
-      return { statusCode: 500, body: "Server error: Missing API password" };
-    }
-
-    const xml = `
-      <createPacket>
-        <apiPassword>${apiPassword}</apiPassword>
-        <packetAttributes>
-          <number>${data.number}</number>
-          <name>${data.name}</name>
-          <surname>${data.surname}</surname>
-          <email>${data.email}</email>
-          <phone>${data.phone}</phone>
-          <addressId>${data.addressId}</addressId>
-          <cod>${data.cod}</cod>
-          <value>${data.value}</value>
-          <weight>${data.weight}</weight>
-          <eshop>${data.eshop}</eshop>
-        </packetAttributes>
-      </createPacket>`;
-
-console.log("Sending XML to Packeta:", xml);
-const response = await fetch("https://api.packeta.com/v1/packets", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/xml",
-  },
-  body: xml,
-});
-
-
-    if (!response.ok) {
-      const errorText = await response.text();
-      console.error("Packeta API error:", errorText);
-      return { statusCode: response.status, body: errorText };
-    }
-
-
-console.log("Packeta response status:", response.status);
-const result = await response.text();
-console.log("Packeta response text:", result);
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ success: true, response: result }),
-    };
-  } catch (err) {
-    console.error("Server error:", err);
-    return { statusCode: 500, body: JSON.stringify({ error: err.message }) };
-  }
+  return {
+    statusCode: 200,
+    body: "Function is working"
+  };
 };
