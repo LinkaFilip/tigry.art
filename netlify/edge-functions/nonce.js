90cfb311e9544580fa6cf514d9fa3e3f4fcfbd37
LinkaFilip <filip.linka@email.cz>
2025-08-12 17:14:14 +0200

    changing to nonce

---
+++ netlify/edge-functions/nonce.js
@@ -0,0 +1,18 @@
+export default async (request, context) => {
+  const nonce = crypto.randomUUID().replace(/-/g, "");
+  let response = await context.next();
+  response.headers.set(
+    "Content-Security-Policy",
+    `script-src 'self' 'nonce-${nonce}'; style-src 'self' 'nonce-${nonce}'; object-src 'none'; base-uri 'self';`
+  );
+  if (response.headers.get("content-type")?.includes("text/html")) {
+    let body = await response.text();
+    body = body.replaceAll("<script", `<script nonce="${nonce}"`);
+    body = body.replaceAll("<style", `<style nonce="${nonce}"`);
+    response = new Response(body, response);
+  }
+
+  return response;
+};
+
+export const config = { path: "/*" };
